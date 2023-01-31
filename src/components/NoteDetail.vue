5d2377dd2fd28e8d7bd47f8ecba3d0ede4cf6dc3
Jack <1162858020@qq.com>
2023-01-31 16:15:22 +0800

    代码为课程链接代码npm install 后

---
+++ src/components/NoteDetail.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="note-detail">
+    <h1>{{msg}} : {{ $route.params.noteId }}</h1>
+  </div>
+</template>
+
+<script>
+import Auth from '@/apis/auth'
+
+export default {
+  data () {
+    return {
+      msg: '笔记详情列表'
+    }
+  },
+
+  created() {
+    Auth.getInfo()
+      .then(res => {
+        if(!res.isLogin) {
+          this.$router.push({ path: '/login' })
+        }
+      })
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
